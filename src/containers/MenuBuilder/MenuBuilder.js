ee45a9c7f6cad21f1b3373d47dbfc30b11ec09c0
TamerB <tamer.bhgt@gmail.com>
2019-10-26 20:18:49 +0200

    Replaced using state with useState functionality

--- src/containers/MenuBuilder/MenuBuilder.js
+++ src/containers/MenuBuilder/MenuBuilder.js
@@ -1,82 +1,70 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 
 import Aux from '../../hoc/Aux';
 import Menu from '../../components/Menu/Menu';
 import Cockpit from '../../components/Cockpit/Cockpit';
 import MenuForm from '../../components/MenuForm/MenuForm';
 
-class MenuBuilder extends Component {
-  state = {
-    menu: [
-      {id: 1, name: "Pizza Margherita", type: "MAIN COURSE", price: 5, photo: ''},
-      {id: 2, name: "Pizza Margherita", type: "MAIN COURSE", price: 5, photo: ''},
-      {id: 3, name: "Pizza Margherita", type: "MAIN COURSE", price: 5, photo: ''}
-    ],
-    temp: {id: 4, name: '', type: '', price: '', photo: ['', '']},
-    showMenu: true,
-    loading: false,
-  }
+function MenuBuilder () {
+  const [enteredMenu, setEnteredMenu] = useState([]);
+  const [id, setid] = useState(0);
+  const [type, settype] = useState('');
+  const [name, setname] = useState('');
+  const [price, setprice] = useState('');
+  const [photo, setphoto] = useState('');
+  const [photoClass, setphotoClass] = useState('blue');
+  const [showMenu, setShowMenu] = useState(true);
 
-  submitHandler = (event) => {
+  const submitHandler = (event) => {
     event.preventDefault();
-    this.setState( { loading: true } );
-    let submitted = {id: this.state.temp.id, type: this.state.temp.type, name: this.state.temp.name, price: this.state.temp.price, photo: this.state.temp.photo[0]};
-    if (this.isValid(submitted)) {
-      this.setState({menu: [...this.state.menu, submitted]});
-      let counter = this.state.temp.id+1;
-      this.setState({temp: {
-        id: counter,
-        type: [''],
-        name: [''],
-        price: [''],
-        photo: ['', '']
-      }});
-      this.toggleFormHandler();
+    let submitted = {id: id, type: type, name: name, price: price, photo: photo};
+
+    if (isValid(submitted)) {
+      setEnteredMenu(enteredMenu => ([...enteredMenu, submitted]));
+
+      setid(id + 1);
+      settype('');
+      setname('');
+      setprice('');
+      setphoto('');
+      setphotoClass('blue');
+      
+      toggleFormHandler();
     }
   }
 
-  validatePhoto = () => {
-    let red = this.state.temp.photo[0] === '' ? 'red' : '';
-    let temp = {id: this.state.temp.id, type: this.state.temp.type, name: this.state.temp.name, price: this.state.temp.price, photo: [this.state.temp.photo[0], red]};
-    this.setState({temp: temp});
+  const validatePhoto = () => {
+    setphotoClass(photo === '' ? 'red' : 'blue');
   }
 
-  isValid = (temp) => {
-    for (let value in temp) {
-      if (temp[value] === '')
-        return false;
-    }
-    return true;
+  const isValid = (temp) => {
+    return eval(temp) !== '';
   }
 
-  toggleFormHandler = () => {
-    const doesShow = this.state.showMenu;
-    this.setState({showMenu: !doesShow});
+  const toggleFormHandler = () => {
+    const doesShow = showMenu;
+    setShowMenu(!doesShow);
   }
 
-  changeFormValue = (event, tagType) => {
-    let temp = {...this.state.temp};
+  const changeFormValue = (event, tagType) => {
+
     if (tagType === 'photo') {
-      temp.photo[0] = /[png|PNG|jpg|JPG|jpeg|JPEG]$/.test(event.target.value) ? event.target.value : '';
-      temp.photo[1] = temp.photo[0] === '' ? 'red' : '';
+      let temp = /[png|PNG|jpg|JPG|jpeg|JPEG]$/.test(event.target.value) ? event.target.value : '';
+      setphoto(temp);
+      setphotoClass(event.target.value === '' ? 'red' : 'blue');
     } else if (tagType === 'price') {
-      temp[tagType] = Number(event.target.value) || '';
+      setprice(Number(event.target.value) || '');
     } else {
-      temp[tagType] = event.target.value;
+      eval('set' + tagType)(event.target.value);
     }
-    this.setState({temp: temp});
   }
 
-  render () {
-    let menu = <Menu menu={this.state.menu} />
-    let menuForm = <MenuForm formValues={this.state.temp} changed={(event, tagType) => this.changeFormValue(event, tagType)} validatePhoto={this.validatePhoto} submitHandler={this.submitHandler} />
-    return (
+  return (
       <Aux>
-        <Cockpit showButton={this.state.showMenu} clicked={this.toggleFormHandler} />
-        {this.state.showMenu ? menu : menuForm}
+        <Cockpit showButton={showMenu} clicked={toggleFormHandler} />
+        {showMenu ? (enteredMenu.length > 0 ? <Menu menu={enteredMenu} /> : <p>This menu is empty.</p>) : <MenuForm formValues={{type: type, name: name, price: price, photo: [photo, photoClass]}} changed={(event, tagType) => changeFormValue(event, tagType)} validatePhoto={validatePhoto} submitHandler={submitHandler} />}
       </Aux>
     );
-  }
 }
 
 export default MenuBuilder;
